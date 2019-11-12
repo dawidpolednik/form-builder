0872b9adc52db774584c3f83808d7398a4741671
Dawid <dawid.polednik@gmail.com>
2019-11-12 10:45:08 +0100

    ..

---
+++ src/Components/RenderButton/RenderButton.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import styles from "./RenderButton.module.scss";
+
+class RenderButton extends Component {
+  render() {
+    return <button>Generate</button>;
+  }
+}
+export default RenderButton;
